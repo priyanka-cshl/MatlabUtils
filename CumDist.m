5e3f16df7697f84dada093d21fc72ae4bbf4488c
priyanka-cshl <speak2priya@gmail.com>
2022-02-14 17:21:54 -0500

    bug fix

--- CumDist.m
+++ CumDist.m
@@ -1,8 +1,11 @@
 function [MyDist] = CumDist(YVar,XVar)
 	MyDist = [];
-	for y = 1:numel(YVar)
-		for x = 1:numel(XVar)
-			MyDist(x,y) = find(numel(find(YVar(:,y)<=XVar(x))));
-		end
+	for y = 1:size(YVar,2)
+		for x = 1:numel(XVar)-1
+			MyDist(x,y) = numel(find(YVar(:,y)<=XVar(x)));
+        end 
+        x = x+1;
+        MyDist(x,y) = MyDist(x-1,y) + numel(find(YVar(:,y)>XVar(x)));
+        MyDist(:,y) = MyDist(:,y)/max(MyDist(:,y));
 	end
 end
