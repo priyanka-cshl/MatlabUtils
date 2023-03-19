89d41e5d90408ce8b36966a5fa13d9af325211bb
Marie Dussauze <mdussauz@cshl.edu>
2023-03-19 11:06:57 -0400

    minor - paths for different computers

--- WhichComputer.m
+++ WhichComputer.m
@@ -46,21 +46,32 @@ switch computername
         Paths.Grid.Ephys{3}             = '/Users/Priyanka/Desktop/LABWORK_II/Data/Ephys';
         Paths.Local.Ephys_processed     = '/Users/Priyanka/Desktop/LABWORK_II/Data/Smellocator/Processed/Ephys';
         Paths.Grid.Ephys_processed      = Paths.Local.Ephys_processed;
-    case 'DESKTOP-A6NSB2F' %Marie remote
+    case 'DESKTOP-SBT7SLO' %Marie remote desktop
         Paths.Code                      = 'C:\Users\Marie\Documents\Code'; % where all code files are
-        Paths.Grid.Behavior             = 'Z:\pgupta\Behavior'; % raw behavior, tuning mtalab files
-        Paths.Grid.Ephys{1}             = 'Z:\pgupta\EphysData'; % raw oeps files - for PCX batch
-        Paths.Grid.Ephys{2}             = 'Z:\mdussauz\ephysdata\lever_task\Batch'; % raw oeps files - for batch O,MO, J
-        Paths.Local.Ephys_processed     = 'C:\Users\Marie\Documents\data\Sorted'; % local copy where sorted, curated spike data is stored
-        Paths.Grid.Ephys_processed      = 'Z:\mdussauz\Smellocator\Processed\Ephys'; % local copy where sorted, curated spike data is stored
-        Paths.Local.Behavior_processed  = 'C:\Users\Marie\Documents\data\Behavior'; % local copy where sorted, curated spike data is stored
-        Paths.Grid.Behavior_processed   = 'Z:\mdussauz\Smellocator\Processed\Behavior'; % local copy where sorted, curated spike data is stored
+        Paths.Grid.Behavior             = 'Y:\pgupta\Behavior'; % raw behavior, tuning mtalab files
+        Paths.Grid.Ephys{1}             = 'Y:\pgupta\EphysData'; % raw oeps files - for PCX batch
+        Paths.Grid.Ephys{2}             = 'Y:\mdussauz\ephysdata\lever_task\Batch'; % raw oeps files - for batch O,MO, J
+        Paths.Local.Ephys_processed     = 'C:\Users\Marie\Documents\data\Smellocator\Processed\Ephys'; % local copy where sorted, curated spike data is stored
+        Paths.Grid.Ephys_processed      = 'Y:\mdussauz\Smellocator\Processed\Ephys'; % local copy where sorted, curated spike data is stored
+        Paths.Local.Behavior_processed  = 'C:\Users\Marie\Documents\data\Smellocator\Processed\Behavior'; % local copy where sorted, curated spike data is stored
+        Paths.Grid.Behavior_processed   = 'Y:\mdussauz\Smellocator\Processed\Behavior'; % local copy where sorted, curated spike data is stored
+        Paths.ProcessedSessions         = Paths.Grid.Behavior_processed;
         
         % LocationMapping Experiments done by Blom on AON mice
-        Paths.Mapping.EphysRaw = 'Z:\pgupta\EphysData\odor_location';
-        Paths.Mapping.EphysSorted = 'Z:\pgupta\EphysData\odor_location\Sorted';
-        Paths.Mapping.StimulusFile = 'Z:\pgupta\Behavior';
-        Paths.Mapping.Processed = 'Z:\mdussauz\Smellocator\Processed\Mapping';
+        Paths.Mapping.EphysRaw = 'Y:\pgupta\EphysData\odor_location';
+        Paths.Mapping.EphysSorted = 'Y:\pgupta\EphysData\odor_location\Sorted';
+        Paths.Mapping.StimulusFile = 'Y:\pgupta\Behavior';
+        Paths.Mapping.Processed = 'Y:\mdussauz\Smellocator\Processed\Mapping';
+    case 'MarieMacCSHL' %Marie laptop
+        Paths.Code                      = '/Users/Marie/Desktop/Analysis/Code'; % where all code files are
+        Paths.Grid.Behavior             = 'albeanu_nlsas_norepl_data/pgupta/Behavior'; % raw behavior, tuning mtalab files
+        Paths.Grid.Ephys{1}             = 'albeanu_nlsas_norepl_data/pgupta/EphysData'; % raw oeps files - for PCX batch
+        Paths.Grid.Ephys{2}             = 'albeanu_nlsas_norepl_data/mdussauz/ephysdata/lever_task/Batch'; % raw oeps files - for batch O,MO, J
+        Paths.Local.Ephys_processed     = '/Users/Marie/Desktop/Analysis/data/Smellocator/Processed/Ephys'; % local copy where sorted, curated spike data is stored
+        Paths.Grid.Ephys_processed      = 'albeanu_nlsas_norepl_data/mdussauz/Smellocator/Processed/Ephys'; % local copy where sorted, curated spike data is stored
+        Paths.Local.Behavior_processed  = '/Users/Marie/Desktop/Analysis/data/Smellocator/Processed/Behavior'; % local copy where sorted, curated spike data is stored
+        Paths.Grid.Behavior_processed   = 'albeanu_nlsas_norepl_data/mdussauz/Smellocator/Processed/Behavior'; % local copy where sorted, curated spike data is stored
+        Paths.ProcessedSessions         = Paths.Grid.Behavior_processed;
 end
 
 
