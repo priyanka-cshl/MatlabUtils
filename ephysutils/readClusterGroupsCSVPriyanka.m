55b8c9d2a943ffc3e11aabb8922e981ca0fc04d5
priyanka-cshl <speak2priya@gmail.com>
2023-05-24 16:33:41 -0400

    better handling of phy outputs

--- ephysutils/readClusterGroupsCSVPriyanka.m
+++ ephysutils/readClusterGroupsCSVPriyanka.m
@@ -1,5 +1,3 @@
-
-
 function [cids, cgs, wires] = readClusterGroupsCSVPriyanka(filename)
 %function [cids, cgs] = readClusterGroupsCSV(filename)
 % cids is length nClusters, the cluster ID numbers
@@ -29,15 +27,35 @@ cgs(isUns) = 3;
 % also read which wire it was strongest on
 fid = fopen(regexprep(filename,'_group','_info'));
 
-
-
 % N = 8;
 % N = numel(regexp(headers,'\t'))+1; % no. of columns
-headers = fgetl(fid);
-%formatSpec = '%s';
-%headers = textscan(fid,formatSpec,N);
+headers = strsplit(fgetl(fid));
+% foo = strsplit(headers);
+% token = [repmat('%f ',1,numel(foo)),'%*[^\n]'];
 
-foo = textscan(fid,'%f %f %f %*[^\n]');
-%wires = [cell2mat(foo(1)) cell2mat(foo(3))];
-wires = [cell2mat(foo(1)) cell2mat(foo(3)) cell2mat(foo(2))]; % also read amplitude
+for i = 1:numel(cids)
+    attributes = strsplit(fgetl(fid));
+        
+    wires(i,1:4) = [str2double(attributes{find(cellfun(@(x) strcmp(x,'cluster_id'), headers))}) ...
+        str2double(attributes{find(cellfun(@(x) strcmp(x,'ch'), headers))}) ...
+        str2double(attributes{find(cellfun(@(x) strcmp(x,'amp'), headers))}) ...
+        str2double(attributes{find(cellfun(@(x) strcmp(x,'fr'), headers))}) ];
+    wires(i,5:7) = NaN;
+    % append extra info if it exists
+    if ~isempty(find(cellfun(@(x) strcmp(x,'fractionRPV'), headers)))
+        wires(i,5) = str2double(attributes{find(cellfun(@(x) strcmp(x,'fractionRPV'), headers))});
+    end
+    if ~isempty(find(cellfun(@(x) strcmp(x,'ISIViolations'), headers)))
+        wires(i,6) = str2double(attributes{find(cellfun(@(x) strcmp(x,'ISIViolations'), headers))});
+    end
+    if ~isempty(find(cellfun(@(x) strcmp(x,'n_spikes'), headers)))
+        wires(i,7) = str2double(attributes{find(cellfun(@(x) strcmp(x,'n_spikes'), headers))});
+    end
+end
+% %formatSpec = '%s';
+% %headers = textscan(fid,formatSpec,N);
+% 
+% foo = textscan(fid,'%f %f %f %f %f %*[^\n]');
+% %wires = [cell2mat(foo(1)) cell2mat(foo(3))];
+% wires = [cell2mat(foo(1)) cell2mat(foo(3)) cell2mat(foo(2))]; % also read amplitude
 fclose(fid);
