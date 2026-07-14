6b6c90d94ccf042a4ead5ff8ab6be0dcff4183ff
priyanka-cshl <21067042+priyanka-cshl@users.noreply.github.com>
2026-07-14 12:57:42 -0400

    minor

--- ephysutils/LoadKS4Units.m
+++ ephysutils/LoadKS4Units.m
@@ -10,16 +10,22 @@ params.addParameter('allUnits', 0, @(x) isnumeric(x)); % default is load only go
 % extract values from the inputParser
 params.parse(varargin{:});
 minRate = params.Results.minSpikes; % in sec
-allUnits = (params.Results.allUnits == 1);
+allUnits = params.Results.allUnits;
 
 %% load KS4 units
 load(fullfile(myKsDir,'quickprocesssniffs.mat')); % sniff times, KS4Units
 if exist('KS4Units') %&& isempty(dir(fullfile(myKsDir,'kilosort4','cluster_info*')))
     myUnits = [[KS4Units.id]' [KS4Units.tetrode]' [KS4Units.quality]' [KS4Units.spikecount]'];
     myUnits(:,end+1) = 1:size(myUnits,1);
-    if ~allUnits
-        % session wasn't curated in phy, keep only 'good' units
-        myUnits(find(myUnits(:,3)~=2),:) = [];
+    switch allUnits
+        case 0
+            % keep only good
+            myUnits(find(myUnits(:,3)~=2),:) = [];
+        case 1
+            % keep all
+        case -1
+            % keep only non-good
+            myUnits(find(myUnits(:,3)==2),:) = [];
     end
     disp(['found ',num2str(size(myUnits,1)),' good units']);
     SingleUnits = KS4Units(myUnits(:,end));
